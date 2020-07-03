d27e98a605c6e253a46dcd779c6a77486f9d7e70
yumeng <yumenglalala@icloud.com>
2020-07-03 15:13:54 +0800

    修复

--- src/main/java/com/yumeng/utils/collection_utils/ListUtil.java
+++ src/main/java/com/yumeng/utils/collection_utils/ListUtil.java
@@ -3,6 +3,7 @@ package com.yumeng.utils.collection_utils;
 
 import org.apache.poi.ss.formula.functions.T;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,24 +16,22 @@ public final class ListUtil {
 
     /**
      * 根据list里的对象的某个属性去重
-     * 用法示例list.stream().filter(ListUtil.distinctByKey(A::getName)).collect(Collectors.toList());
+     * 用法示例list.stream().filter(ListUtil.distinctByAttr(A::getName)).collect(Collectors.toList());
      * @param keyExtractor A::getName
      * @param <T> 返回值
      * @return
      */
-    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+    public static <T> Predicate<T> distinctByAttr(Function<? super T, Object> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
-
 
     /**
      * 基础字段去重，例如String，Integer
      * @param list
      * @return
      */
-    public static List<?> distinctStringList(List<?> list){
+    public static List<?> distinctBasicList(List<?> list){
         return list.stream().distinct().collect(Collectors.toList());
     }
-
 }
--- src/test/java/com/yumeng/utils/UtilsApplicationTests.java
+++ src/test/java/com/yumeng/utils/UtilsApplicationTests.java
@@ -9,14 +9,15 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.lang.model.util.Elements;
+import javax.swing.text.Document;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.net.InetAddress;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class UtilsApplicationTests {
@@ -89,6 +90,44 @@ class UtilsApplicationTests {
 //        String filename = "https://yuantongbucket.oss-cn-beijing.aliyuncs.com/qrcode/48194f172c2243a8b3076ffa8845dd98.jpg";
 //        String relativePath = filename.substring(filename.lastIndexOf("/",filename.lastIndexOf("/")-1)+1);
 //        System.out.println(relativePath);
+        A a = new A();
+        a.setI(1);
+        A a1 = new A();
+        a1.setI(3);
+        A a2 = new A();
+        a2.setI(2);
+        A a3 = new A();
+        a3.setI(4);
 
+        List<A> list = new ArrayList<>();
+        list.add(a);
+        list.add(a1);
+        list.add(a2);
+        list.add(a3);
+
+
+
+        list = list.stream().sorted(Comparator.comparing(A::getI).reversed())
+                .collect(Collectors.toList());
+
+        for (A a4:list) {
+            System.out.println(a4.getI());
+        }
+
+    }
+
+
+
+
+    class A{
+        private int i;
+
+        public int getI() {
+            return i;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
     }
 }
