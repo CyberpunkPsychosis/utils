64d0610dbe61c698b61c9feadadfacd469e5fbc9
于蒙 <7525144+yumeng7@user.noreply.gitee.com>
2020-12-06 22:31:55 +0800

    修改

--- src/test/java/com/yumeng/utils/UtilsApplicationTests.java
+++ src/test/java/com/yumeng/utils/UtilsApplicationTests.java
@@ -1,5 +1,9 @@
 package com.yumeng.utils;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
+import cn.afterturn.easypoi.excel.entity.enmus.ExcelStyleType;
+import cn.afterturn.easypoi.util.PoiMergeCellUtil;
 import com.yumeng.utils.excel_utils.ExcelConfig;
 import com.yumeng.utils.excel_utils.ExcelParse;
 import com.yumeng.utils.excel_valid_utils.*;
@@ -9,16 +13,18 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.io.*;
+import java.util.*;
 
 @SpringBootTest
 class UtilsApplicationTests {
+
+    private String TEST = "C:\\Users\\yum\\Desktop\\测试\\test.xlsx";
+
+    private String ceshi = "C:\\Users\\yum\\Desktop\\测试";
+
+    private String moban = "C:\\Users\\yum\\Desktop\\模板\\";
 
     //表格校验示例代码
     @Test
@@ -81,5 +87,38 @@ class UtilsApplicationTests {
         ExcelParse excelParse = new ExcelParse(excelConfig);
         List<Map<Integer, Map<Integer, String>>> list = excelParse.resolveExcelWordOnImage(workbook);
         System.out.println(list.toString());
+    }
+
+    @Test
+    void contextLoads3() throws Exception {
+        TemplateExportParams params = new TemplateExportParams(
+                moban + "foreach_insert_merge.xlsx");
+        Map<String, Object> map  = new HashMap();
+        List<Map> list = new ArrayList<>();
+        Map aa = new HashMap() {
+            {
+                put("id", "id" + 0);
+                put("name", "name" + 0);
+                put("age", "age" + 0);
+                put("sex", "sex" + 0);
+            }
+        };
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            list.add(aa);
+        }
+        map.put("nums", "你是一只小小鸟");
+        map.put("list1", list);
+        map.put("list2", list);
+        //本来导出是专业那个
+        Workbook workbook = ExcelExportUtil.exportExcel(params, map);
+        PoiMergeCellUtil.mergeCells(workbook.getSheetAt(0), 1, 0, 4);
+        File savefile = new File(ceshi);
+        if (!savefile.exists()) {
+            savefile.mkdirs();
+        }
+        FileOutputStream fos = new FileOutputStream(TEST);
+        workbook.write(fos);
+        fos.close();
     }
 }
