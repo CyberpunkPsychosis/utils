c535cf3a4b58c690447ecf9faf50cbe60b3bb06e
yumeng <yumenglalala@icloud.com>
2021-01-22 11:48:50 +0800

    修复

--- src/main/java/com/yumeng/utils/UtilsApplication.java
+++ src/main/java/com/yumeng/utils/UtilsApplication.java
@@ -1,11 +1,7 @@
 package com.yumeng.utils;
 
-import com.yumeng.utils.bean_utils.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,10 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class UtilsApplication {
     public static void main(String[] args) {
         SpringApplication.run(UtilsApplication.class, args);
-    }
-
-    @PostMapping("test")
-    public String test(@Validated @RequestBody Test test){
-        return "测试";
     }
 }
